c4198ec1ed5bfbb498ea223cce6ae535cb152795
ManuJS <emanuelle.menali@hotmail.com>
2016-10-12 14:12:02 -0300

    novo adapter

---
+++ app/src/main/java/em/android/sunshine/adapter/ForecastAdapter.java
@@ -0,0 +1,8 @@
+package em.android.sunshine.adapter;
+
+/**
+ * Created by emanu on 11/10/2016.
+ */
+
+public class ForecastAdapter {
+}
---
+++ app/src/main/java/em/android/sunshine/utility/Utility.java
@@ -0,0 +1,8 @@
+package em.android.sunshine.utility;
+
+/**
+ * Created by emanu on 11/10/2016.
+ */
+
+public class Utility {
+}
