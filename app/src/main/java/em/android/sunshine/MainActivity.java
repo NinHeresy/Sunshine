9c706e67f5b0b41bffa64537f2e476f2838898b5
ManuJS <emanuelle.menali@hotmail.com>
2016-12-11 13:43:23 -0200

    erro na orientacao de layout

--- app/src/main/java/em/android/sunshine/MainActivity.java
+++ app/src/main/java/em/android/sunshine/MainActivity.java
@@ -35,23 +35,19 @@ public class  MainActivity extends AppCompatActivity implements SecondFragment.C
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        if (findViewById(R.id.weather_detail_container) != null) {
+        if (findViewById(R.id.container_fragment) != null) {
             mTwoPane = true;
 
                 if (savedInstanceState == null) {
                     getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.weather_detail_container, new DetailActvityFragment(),DETAILFRAGMENT_TAG)
+                            .replace(R.id.container_fragment, new DetailActvityFragment(),DETAILFRAGMENT_TAG)
                             .commit();
                 }
 
         }else{
             mTwoPane = false;
-            getSupportActionBar().setElevation(0f);
         }
 
-        SecondFragment forecastFragment =  ((SecondFragment)getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_forecast));
-        forecastFragment.setUseTodayLayout(!mTwoPane);
 
         SunshineSyncAdapter.initializeSyncAdapter(this);
 
@@ -81,11 +77,32 @@ public class  MainActivity extends AppCompatActivity implements SecondFragment.C
             return true;
 
         }
-//        if(id == R.id.abre_mapa){
-//            abreLocalizacaoNoMapa();
-//            return true;
-//        }
+        if(id == R.id.abre_mapa){
+            abreLocalizacaoNoMapa();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void abreLocalizacaoNoMapa() {
+        String location = Utility.getPreferredLocation(this);
+
+        // Using the URI scheme for showing a location found on a map.  This super-handy
+        // intent can is detailed in the "Common Intents" page of Android's developer site:
+        // http://developer.android.com/guide/components/intents-common.html#Maps
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
     }
 
     @Override
@@ -94,14 +111,11 @@ public class  MainActivity extends AppCompatActivity implements SecondFragment.C
         String location = Utility.getPreferredLocation( this );
         // update the location in our second pane using the fragment manager
         if (location != null && !location.equals(mLocation)) {
-            SecondFragment ff = (SecondFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
+            SecondFragment ff = (SecondFragment)getSupportFragmentManager().findFragmentById(R.id.container);
             if ( null != ff ) {
                 ff.onLocationChanged();
             }
-            DetailActvityFragment df = (DetailActvityFragment)getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
-            if ( null != df ) {
-                df.onLocationChanged(location);
-            }
+
             mLocation = location;
         }
     }
@@ -120,7 +134,7 @@ public class  MainActivity extends AppCompatActivity implements SecondFragment.C
             fragment.setArguments(args);
 
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.weather_detail_container, fragment, DETAILFRAGMENT_TAG)
+                    .replace(R.id.container_fragment, fragment, DETAILFRAGMENT_TAG)
                     .commit();
         } else {
             Intent intent = new Intent(this, DetailActvity.class)
